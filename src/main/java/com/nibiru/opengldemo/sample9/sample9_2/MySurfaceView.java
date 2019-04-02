29732b2046a9b48514ea3dea23004809466e38e1
zkuokuo <1216819573@qq.com>
2019-04-02 18:14:19 +0800

    添加加载obj模型文件显示同时加入光照的案例

--- src/main/java/com/nibiru/opengldemo/sample9/sample9_2/MySurfaceView.java
+++ src/main/java/com/nibiru/opengldemo/sample9/sample9_2/MySurfaceView.java
@@ -1,4 +1,4 @@
-package com.nibiru.opengldemo.sample9.sample9_1;
+package com.nibiru.opengldemo.sample9.sample9_2;
 
 import android.content.Context;
 import android.opengl.GLES30;
@@ -19,11 +19,11 @@ import javax.microedition.khronos.opengles.GL10;
 
 
 class MySurfaceView extends GLSurfaceView {
-    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;//角度换算比例
+    private final float TOUCH_SCALE_FACTOR = 180.0f / 320;//角度缩放比例
     private SceneRenderer mRenderer;//场景渲染器
 
-    private float mPreviousY;//上次的触控位置的Y坐标
-    private float mPreviousX;//上次的触控位置的X坐标
+    private float mPreviousY;//上次的触控位置Y坐标
+    private float mPreviousX;//上次的触控位置X坐标
 
     public MySurfaceView(Context context) {
         super(context);
@@ -33,7 +33,7 @@ class MySurfaceView extends GLSurfaceView {
         setRenderMode(GLSurfaceView.RENDERMODE_CONTINUOUSLY);//设置渲染模式为主动渲染
     }
 
-    //触控事件回调方法
+    //触摸事件回调方法
     @Override
     public boolean onTouchEvent(MotionEvent e) {
         float y = e.getY();
@@ -42,8 +42,8 @@ class MySurfaceView extends GLSurfaceView {
             case MotionEvent.ACTION_MOVE:
                 float dy = y - mPreviousY;//计算触控笔Y位移
                 float dx = x - mPreviousX;//计算触控笔X位移
-                mRenderer.yAngle += dx * TOUCH_SCALE_FACTOR;//设置绕y轴旋转角度
-                mRenderer.xAngle += dy * TOUCH_SCALE_FACTOR;//设置绕x轴旋转角度
+                mRenderer.yAngle += dx * TOUCH_SCALE_FACTOR;//设置沿y轴旋转角度
+                mRenderer.xAngle += dy * TOUCH_SCALE_FACTOR;//设置沿x轴旋转角度
                 requestRender();//重绘画面
         }
         mPreviousY = y;//记录触控笔位置
@@ -54,24 +54,25 @@ class MySurfaceView extends GLSurfaceView {
     private class SceneRenderer implements GLSurfaceView.Renderer {
         float yAngle;//绕Y轴旋转的角度
         float xAngle; //绕X轴旋转的角度
-        //从指定的obj文件中加载的对象
-        LoadedObjectVertexOnly lovo;
+        //从指定的obj文件中加载对象
+        LoadedObjectVertexNormal lovo;
 
         public void onDrawFrame(GL10 gl) {
             //清除深度缓冲与颜色缓冲
             GLES30.glClear(GLES30.GL_DEPTH_BUFFER_BIT | GLES30.GL_COLOR_BUFFER_BIT);
 
-            MatrixState.pushMatrix();//保护现场
-            MatrixState.translate(0, -2f, -25f);//平移坐标系
+            //坐标系推远
+            MatrixState.pushMatrix();
+            MatrixState.translate(0, -2f, -25f);   //ch.obj
             //绕Y轴、X轴旋转
-            MatrixState.rotate(yAngle, 0, 1, 0);//绕Y轴旋转
-            MatrixState.rotate(xAngle, 1, 0, 0);//绕X轴旋转
+            MatrixState.rotate(yAngle, 0, 1, 0);
+            MatrixState.rotate(xAngle, 1, 0, 0);
 
             //若加载的物体不为空则绘制物体
             if (lovo != null) {
                 lovo.drawSelf();
             }
-            MatrixState.popMatrix(); //恢复现场
+            MatrixState.popMatrix();
         }
 
         public void onSurfaceChanged(GL10 gl, int width, int height) {
@@ -94,8 +95,10 @@ class MySurfaceView extends GLSurfaceView {
             GLES30.glEnable(GLES30.GL_CULL_FACE);
             //初始化变换矩阵
             MatrixState.setInitStack();
+            //初始化光源位置
+            MatrixState.setLightLocation(40, 10, 20);
             //加载要绘制的物体
-            lovo = LoadUtil.loadFromFile("sample9_2/ch.obj", MySurfaceView.this.getResources(), MySurfaceView.this);
+            lovo = LoadUtil.loadFromFile("sample9_1/ch.obj", MySurfaceView.this.getResources(), MySurfaceView.this);
         }
     }
 }
