9cd77b538c1f23277bdd380238b8509d62d6bbed
zkuokuo <1216819573@qq.com>
2019-04-02 18:41:50 +0800

    添加加载obj模型文件显示关闭背景剪裁和打开双面关照效果案例

---
+++ src/main/java/com/nibiru/opengldemo/sample9/sample9_6/MainActivity.java
@@ -0,0 +1,47 @@
+package com.nibiru.opengldemo.sample9.sample9_6;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * 作者:dick
+ * 公司:nibiru
+ * 时间:2019-04-02
+ * 描述:
+ */
+
+public class MainActivity extends Activity {
+    private MySurfaceView mGLSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //设置为全屏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //设置为横屏模式
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //初始化GLSurfaceView
+        mGLSurfaceView = new MySurfaceView(this);
+        setContentView(mGLSurfaceView);
+        mGLSurfaceView.requestFocus();//获取焦点
+        mGLSurfaceView.setFocusableInTouchMode(true);//设置为可触控
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGLSurfaceView.onPause();
+    }
+}
