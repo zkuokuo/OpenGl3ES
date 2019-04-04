7f047159b5d351ac95f6468314511d35b1bb5150
zkuokuo <1216819573@qq.com>
2019-04-04 17:04:15 +0800

    添加天空盒案例

---
+++ src/main/java/com/nibiru/opengldemo/sample11/sample11_5/Sample11_5Activity.java
@@ -0,0 +1,50 @@
+package com.nibiru.opengldemo.sample11.sample11_5;
+
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+/**
+ * 作者:dick
+ * 公司:nibiru
+ * 时间:2019-04-04
+ * 描述:
+ */
+
+public class Sample11_5Activity extends Activity {
+    private MySurfaceView mGLSurfaceView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        //设置为全屏
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN ,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //设置为横屏模式
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //初始化GLSurfaceView
+        mGLSurfaceView = new MySurfaceView(this);
+        setContentView(mGLSurfaceView);
+        mGLSurfaceView.requestFocus();//获取焦点
+        mGLSurfaceView.setFocusableInTouchMode(true);//设置为可触控
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mGLSurfaceView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mGLSurfaceView.onPause();
+    }
+}
+
+
+
